4a4b3c79001ffe9dd28140562c7cf645bcda4ca8
arseniyus <r.kozachenko2000@gmail.com>
2024-02-13 11:36:35 +0000

    added info about team

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 alert("Hello World");
+let Arseniy {
+    eyeColor: "blue",
+    height: "179 cm",
+    age: "26",
+    favouriteFood: "whatever's in the fridge",
+};
+
+let Pete {
+    eyeColor: "brown",
+    height: "188 cm",
+    age: "46",
+    favouriteFood: "beef short-ribs"
+};
+
+let Gav {
+    eyeColor: "brown",
+    height: "195 cm",
+    age: "38",
+    favouriteFood: "peperroni pizza?"
+};
