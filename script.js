d798005fc039d735f7755c55d9509d4c2f99f0ee
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-13 11:51:26 +0000

    did some stuff

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-alert("Hello World");
-let Arseniy {
-    eyeColor: "blue",
-    height: "179 cm",
-    age: "26",
-    favouriteFood: "whatever's in the fridge",
+let Arseniy = {
+  eyeColor: "blue",
+  height: 179,
+  age: 26,
+  favouriteFood: "whatever's in the fridge",
+  specialNote: {
+    footballTeams: "Chelsea",
+    highSchool: "Therfield",
+  },
 };
 
-let Pete {
-    eyeColor: "brown",
-    height: "188 cm",
-    age: "46",
-    favouriteFood: "beef short-ribs"
+let Pete = {
+  eyeColor: "brown",
+  height: 188,
+  age: 46,
+  favouriteFood: "beef short-ribs",
+  specialNote: {
+    footballTeams: "null",
+    highSchool: "Turnpike",
+  },
 };
 
-let Gav {
-    eyeColor: "brown",
-    height: "195 cm",
-    age: "38",
-    favouriteFood: "peperroni pizza?"
+let Gav = {
+  eyeColor: "brown",
+  height: 195,
+  age: 38,
+  favouriteFood: "peperroni pizza?",
+  specialNote: {
+    footballTeams: "Tottenham",
+    highSchool: "Wanstead",
+  },
 };
